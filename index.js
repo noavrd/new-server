999c46506d838f6e7410340f7cade0de124ae281
noavrd <noavrd@gmail.com>
2021-02-17 22:27:59 +0200

    add put

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ app.get("/v3/b/:id", (req, res) => {
         res.status(404).json({"message": "Invalid Record ID"});
     }
 });
+
+app.put("/v3/b/:id", (req, res) => {
+    const body = req.body;
+    const id = req.params.id;
+    const binExist = fs.existsSync(`./bins/${id}.json`);
+    if(!binExist) {
+        res.status(404).json({
+            "message": "Bin not found",
+            "success": false
+    });
+    return;
+    }
+    fs.existsSync(`./bins/${id}.json`, body);
+    const successMessage = {
+        success: true,
+        data: body,
+        "version": 1,
+        "parentId": id
+    }   
+    res.send(successMessage);                                                        ")
+})
+
+app.listen(3000, () => {
+    console.log("run")
+});
